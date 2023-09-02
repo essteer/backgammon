4ac2e45a4e122d4cce3bd4c01eeee07575d9770b
essteer <essteer@pm.me>
2023-09-02 16:37:52 +0800

    add helper function and add todo task

--- src/roll_outcomes.py
+++ src/roll_outcomes.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 from helpers.frequencies import compute_frequencies
+from helpers.probabilities import compute_probabilities
+from pprint import pprint
 
 possible_rolls = []
 # Generate possible outcomes from rolling two fair six-sided dice
@@ -12,12 +14,16 @@ for i in range(1, 7):
         else:
             possible_rolls.append([i, j])
 
-# Call helper function to generate dict for frequencies of each possible roll
+# Call function to generate dict for frequencies of each roll in possible_rolls
 frequency_dict = compute_frequencies(possible_rolls)
 
-# Generate a dictionary of probabilities for each move option from count_dict
-probs_dict = {}
+# Call function to generate dict for probabilities of rolls in frequency_dict
+probability_dict = compute_probabilities(frequency_dict)
 
-print(*possible_rolls, sep="\n")
+# TODO write function to return dict of probabilities in simplified fractions (for readability)
+
+# print(*possible_rolls, sep="\n")
+# print("")
+# pprint(frequency_dict)
 print("")
-print(frequency_dict)
+pprint(probability_dict)
