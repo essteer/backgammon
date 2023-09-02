aab22b6e8c27919a327e821e34987c750ab42dcd
essteer <essteer@pm.me>
2023-09-02 15:52:15 +0800

    expand directory structure to store helper functions

---
+++ src/helpers/frequencies.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from helpers.powerset import powerset
+
+
+def compute_frequencies(possible_rolls) -> list:
+    """
+    Takes a list of lists of integers, containing possible outcomes from two fair six-sided dice
+    Calls the itertools powerset function to generate a powerset of those outcomes
+    Calculates the sum of each non-blank subset
+    Returns a dictionary of:
+    - key: an integer that can be reached from at least 1 roll combination
+    - value: an integer of the number of combinations that can result in the key integer 
+    """
+    # Create empty dict to store counts of each move option
+    frequency_dict = {}
+    # Iterate through roll combinations and add occurrences to count_dict
+    for roll in possible_rolls:
+
+        choices = []
+        roll_combinations = powerset(roll)
+
+        for x in roll_combinations:
+            # Avoid double-counting:
+            # e.g., a roll of two fours should permit the move 4 to be counted exactly once
+            if sum(list(x)) not in choices:
+                choices.append(sum(list(x)))
+
+        for choice in choices[1:]:  # Skip blank subsets
+            try:
+                # Iterate value by 1
+                frequency_dict[choice] += 1
+            except KeyError:
+                # Create new key, value pair if key not found
+                frequency_dict[choice] = 1
+
+    return frequency_dict
--- src/roll_outcomes.py
+++ src/roll_outcomes.py
@@ -1,46 +1,24 @@
 # -*- coding: utf-8 -*-
-import random
-from itertools import combinations, chain
-
+from helpers.frequencies import compute_frequencies
 
 possible_rolls = []
-
-# Generate possible outcomes from rolling two dice
+# Generate possible outcomes from rolling two fair six-sided dice
 for i in range(1, 7):
     for j in range(1, 7):
         if i == j:
+            # Backgammon rules: doubles yield four moves
+            # E.g. rolling [3, 3] gives a player the move options [3, 3, 3, 3]
             possible_rolls.append([i]*4)
         else:
             possible_rolls.append([i, j])
 
-
-def powerset(iterable):
-    "powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
-    s = list(iterable)
-    return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
+# Call helper function to generate dict for frequencies of each possible roll
+frequencies_dict = compute_frequencies(possible_rolls)
 
 
-# Create empty dict to store move probabilities
-count_dict = {}
-# Iterate through roll combinations and add occurrences to count_dict
-for roll in possible_rolls:
+# Generate a dictionary of probabilities for each move option from count_dict
+probs_dict = {}
 
-    choices = []
-    roll_combinations = powerset(roll)
-
-    for x in roll_combinations:
-        # Avoid double-counting:
-        # e.g., a roll of two fours should permit the move 4 to be counted exactly once
-        if sum(list(x)) not in choices:
-            choices.append(sum(list(x)))
-
-    for choice in choices[1:]:  # Skip blank subsets
-        try:
-            count_dict[choice] += 1
-        except KeyError:
-            count_dict[choice] = 1
-
-
-# print(possible_rolls)
+print(*possible_rolls, sep="\n")
 print("")
-print(count_dict)
+print(frequencies_dict)
