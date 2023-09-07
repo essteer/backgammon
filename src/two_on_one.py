2c7db8b1373caa5bbfc85380d758a6639cc58d6c
essteer <essteer@pm.me>
2023-09-08 00:46:01 +0800

    add two dice probability calculation

--- src/two_on_one.py
+++ src/two_on_one.py
@@ -1,36 +1,24 @@
 # -*- coding: utf-8 -*-
 from helpers.rolls import compute_rolls
-from helpers.frequencies import compute_frequencies
-from helpers.probabilities import compute_probabilities
-from pprint import pprint
 
 # Call function to generate list of lists of possible roll outcomes
 # Defaults to two six-sided dice
 possible_rolls = compute_rolls()
 
-# Call function to generate dict for frequencies of each roll in possible_rolls
-frequency_dict = compute_frequencies(possible_rolls)
-
-# Call function to generate dict for probabilities of rolls in frequency_dict
-probability_dict = compute_probabilities(frequency_dict)
-
-# print("")
-# pprint(frequency_dict)
-# print("")
-# pprint(probability_dict)
-
 # Set number of spaces needed to land a player's pieces on their opponent's piece:
-distance_1 = 5
-distance_2 = 6
+distance_1 = 9  # later replace with input()
+distance_2 = 16  # later replace with input()
+targets = [distance_1, distance_2]
 
-prob_1 = probability_dict[distance_1]
-prob_2 = probability_dict[distance_2]
+count = 0
+for roll in possible_rolls:
+    if len(roll) != 2:
+        combinations = [roll[0]*(i+1) for i in range(len(roll))]
+        if targets[0] in combinations or targets[1] in combinations:
+            count += 1
+
+    elif targets[0] in roll or targets[1] in roll or sum(roll) in targets:
+        count += 1
 
 print(
-    f"Prob. of landing on opponent's piece {distance_1} spaces away: {prob_1:.2f}")
-print(
-    f"Prob. of landing on opponent's piece {distance_2} spaces away: {prob_2:.2f}")
-print("")
-
-# TODO setup calc for prob of rolling either distance_1 OR distance 2
-# note that rolling e.g. [5, 6] should not be counted twice
+    f"Prob. able to land from either {distance_1} or {distance_2} spaces away: {100*(count/36):.2f}%")
