c52b1fe07732ec6a6131b9015a607a8c4cdc9478
essteer <essteer@pm.me>
2023-09-08 00:39:20 +0800

    add print output and keys list - remove comments

--- src/roll_outcomes.py
+++ src/roll_outcomes.py
@@ -14,15 +14,10 @@ frequency_dict = compute_frequencies(possible_rolls)
 # Call function to generate dict for probabilities of rolls in frequency_dict
 probability_dict = compute_probabilities(frequency_dict)
 
+# Create sorted list of keys in probability_dict
+probability_dict_keys = [k for k in probability_dict]
+probability_dict_keys.sort()
 
-# print(*possible_rolls, sep="\n")
-# print("")
-pprint(frequency_dict)
-# print("")
-# pprint(probability_dict)
-
-# TODO write function to return dict of probabilities in simplified fractions (for readability)
-
-# From two_on_one.py:
-# TODO setup calc for prob of rolling either distance_1 OR distance 2
-# note that rolling e.g. [5, 6] should not be counted twice
+for k in probability_dict_keys:
+    print(
+        f"Prob. able to land {k} space{'s' if k != 1 else ''} away: {100*probability_dict[k]:.2f}%")
