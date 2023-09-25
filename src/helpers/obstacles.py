6077d82b90174260510f2e13306abc716bad0606
essteer <essteer@pm.me>
2023-09-25 23:45:02 +0800

    update obstacle helper function

--- src/helpers/obstacles.py
+++ src/helpers/obstacles.py
@@ -62,7 +62,10 @@ def add_obstacles(possible_rolls: list, distance_1: int, distance_2: int = 0, ob
 
 def filter_rolls(possible_rolls: list, obstacles: list) -> list:
     """
-
+    Helper function for add_rolls
+    Takes possible_rolls and obstacles lists
+    Filters out moves that cannot be made due to obstacles
+    Returns reduced list of filtered_rolls
     """
     filtered_rolls = []
 
@@ -71,10 +74,19 @@ def filter_rolls(possible_rolls: list, obstacles: list) -> list:
         if set(roll[:]).issubset(obstacles):
             # Omit ith roll since obstacles can't be avoided by changing move order
             continue
-        # elif len(roll) != 2: # TODO
-            # For doubles if space 9 is blocked, [3, 3, 3, 3]
-            # effectively becomes just [3, 3], since the third and fourth [3] are inaccessible
-            # use modulo here to process TODO
+        elif len(roll) != 2:
+            # For doubles, if e.g. space 9 is blocked, [3, 3, 3, 3]
+            # in effect becomes [3, 3], since the 3rd and 4th [3] are inaccessible
+            if roll[0]*2 in obstacles:  # replace if elif
+                # If sum obstructed, set one to 0 but keep first value TODO
+                filtered_rolls.append([roll[0], 0])
+            elif roll[0]*3 in obstacles:
+                filtered_rolls.append(roll[:2])
+            elif roll[0]*4 in obstacles:
+                filtered_rolls.append(roll[:3])
+
+            filtered_rolls.append(roll)
+
         else:
             filtered_rolls.append(roll)
 
