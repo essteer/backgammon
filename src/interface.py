b5511e4f491dfe8e6b5bc4784d8018b3c807cbb5
essteer <essteer@pm.me>
2023-10-20 16:27:31 +0800

    add code to compute probabilities for distance pairs

--- src/interface.py
+++ src/interface.py
@@ -4,11 +4,16 @@ from helpers.frequencies import compute_frequencies
 from helpers.probabilities import compute_probabilities
 from helpers.combinations import combination_moves
 from helpers.obstacles import add_obstacles
+import numpy as np
 import pandas as pd
 
 DISTANCE_1 = 3
 DISTANCE_2 = 13
 OBSTACLES = [1, 2]
+# Rounding digits
+R = 4
+# Test distances for assert statements
+TESTS = {"A": 7, "B": 13, "C": 10, "D": 15, "E": 4}
 
 ##########################################################################
 # Create data
@@ -25,48 +30,97 @@ stats_list = [[key, freq_dict[key], prob_dict[key]]
               for key in sorted(freq_dict.keys())]
 
 ##########################################################################
-# Sense checks
+# Sense check
 ##########################################################################
 
+# ~~~ Standard probabilities ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 # 6*6 = 36 possible outcomes from two six-sided dice 
-assert len(possible_rolls)   == 36
+assert len(possible_rolls) == 36
 # 17 distinct value outcomes:
 #     1 to 12 via standard die values
 #     15, 16, 18, 20, 24 via doubles rule
 assert len(freq_dict) == 17
 assert len(prob_dict) == 17
-# 1/36 chance of double outcomes
-assert round(1/36, 4) == prob_dict[15] == prob_dict[16] == prob_dict[18] == prob_dict[20] == prob_dict[24]
+# P = 1/36 for extended double outcomes
+assert round(1/36, R) == prob_dict[15] == prob_dict[16] == prob_dict[18] == prob_dict[20] == prob_dict[24]
+
+# ~~~ Combined probabilities ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# P(A or B) == P(A), where P(B) == 0
+assert combination_moves(possible_rolls, TESTS["A"], TESTS["B"])/36 == freq_dict[TESTS["A"]]/36
+# P(A or B) == P(B), where P(A) == 0
+assert combination_moves(possible_rolls, TESTS["B"], TESTS["A"])/36 == freq_dict[TESTS["A"]]/36
+# P(A or B) == P(A) == P(B), where A == B
+assert combination_moves(possible_rolls, TESTS["A"], TESTS["A"])/36 == freq_dict[TESTS["A"]]/36
+
+# P(C or D) == P(C) + P(D) - P(C and D)
+f_C = freq_dict[TESTS["C"]]  # C achieved via [4, 6], [5, 5], or [6, 4]
+f_D = freq_dict[TESTS["D"]]  # D achieved via [5, 5]
+n = 1  # (C and D) achieved via single case [5, 5]
+assert combination_moves(possible_rolls, TESTS["C"], TESTS["D"]) == f_C + f_D - n
+f_A = freq_dict[TESTS["A"]]  # A achieved via 15 combinations
+f_E = freq_dict[TESTS["E"]]  # E achieved via 6 combinations
+n = 2  # (A and E) achieved via two cases [3, 4], [4, 3]
+assert combination_moves(possible_rolls, TESTS["A"], TESTS["E"]) == f_A + f_E - n
 
 print("Sense checks passed")
 
 ##########################################################################
-# Create DataFrame of individual probabilities
+# Create DataFrames
 ##########################################################################
+
+# ~~~ Standard probabilities ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 # Create DataFrame for stats_list
 columns = ["Move", "Frequency", "Probability"]
-df = pd.DataFrame(stats_list, columns=columns)
+standard_df = pd.DataFrame(stats_list, columns=columns)
 # Save DataFrame to csv
-# df.to_csv("./data/backgammon_stats.csv")
+# standard_df.to_csv("./data/backgammon_stats.csv")
+
+# ~~~ Combined probabilities ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# List of possible distances
+distances = np.array([key for key in sorted(freq_dict.keys())])
+# Create meshgrid of all distance combinations
+distance_1, distance_2 = np.meshgrid(distances, distances)
+
+results = []
+# Iterate through each combination of distances
+for d1, d2 in zip(distance_1.flatten(), distance_2.flatten()):
+    # Calculate combined frequencies for distance pair
+    combined_freq = combination_moves(possible_rolls, d1, d2)
+    # Calculate combined probabilities for distance pair
+    combined_prob = np.round(combined_freq / 36, R)
+    # Append to results
+    results.append([d1, d2, combined_prob])
+
+# Create DataFrame for combined_probabilities
+combined_df = pd.DataFrame(results, columns=['Distance 1', 'Distance 2', 'Combined Probability'])
+# Pivot the DataFrame
+pivot_cdf = combined_df.pivot(index='Distance 1', columns='Distance 2', values='Combined Probability')
+pivot_cdf = pivot_cdf.fillna(0.)
+
+# Save DataFrame to csv
+pivot_cdf.to_csv("./data/combined_probs.csv", index=False)
 
 ##########################################################################
 # Query data
 ##########################################################################
 
 # Print probabilities for possible moves between 1 <= x <= 24
-for stat in stats_list:
-    print(
-        f"Prob. able to land {stat[0]} space{'s' if stat[0] != 1 else ''} away: {stat[2]:.2%}")
+# for stat in stats_list:
+#     print(
+#         f"Prob. able to land {stat[0]} space{'s' if stat[0] != 1 else ''} away: {stat[2]:.2%}")
 
 # Test probabilities for reaching two distances
-combination_frequencies = combination_moves(
-    possible_rolls,
-    DISTANCE_1,
-    DISTANCE_2)
+# combination_frequencies = combination_moves(
+#     possible_rolls,
+#     DISTANCE_1,
+#     DISTANCE_2)
 
-print(
-    f"Prob. able to land from either {DISTANCE_1} or {DISTANCE_2} spaces away: {combination_frequencies/36:.2%}")
+# print(
+#     f"Prob. able to land from either {DISTANCE_1} or {DISTANCE_2} spaces away: {combination_frequencies/36:.2%}")
 
 
 # Test use of obstacles
@@ -78,7 +132,3 @@ print(
 
 # print(
 #     f"Prob. able to land from either {DISTANCE_1} or {DISTANCE_2} spaces away: {obstructed_combination_frequencies/36:.2%}")
-
-##########################################################################
-# Sense checks
-##########################################################################
